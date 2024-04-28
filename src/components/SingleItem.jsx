4bccd602ef761d09fa58a65ef940c6dbb91191f5
khalid586 <khalidcomilla58@gmail.com>
2024-04-28 18:06:45 +0600

    design issue fix and modification

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-import { MdOutlineBedroomParent } from "react-icons/md";
+import { MdOutlineBedroomParent, MdOutlineDelete } from "react-icons/md";
 import { FaLandmark } from "react-icons/fa6";
+import { GrUpdate } from 'react-icons/gr';
 
 
 function SingleItem({place , handleDelete}) {
@@ -20,35 +21,35 @@ function SingleItem({place , handleDelete}) {
         travelTime,
     } = place; 
 
-    description =description.split(' ').slice(0, 15).join(' ');
+    description =description.split('').slice(0, 80).join('');
   return (
       
 
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <img className='w-full rounded-t-lg' style={{ height:'250px'}} src = {photoUrl} alt="" />
+    <img className='w-full rounded-t-lg' style={{ height:'280px'}} src = {photoUrl} alt="" />
     <div class="p-5">
         
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
         
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
-        <div className='flex gap-2'>
-            <Link to = {`/details/${_id}`} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Details
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </Link>
+        <div className='flex gap-2 mt-6 mb-4'>
             {
-                handleDelete &&
-                <div className='flex gap-2'>
-                    <Link to ={ `/update/${place._id}`} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-400 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Update 
+                handleDelete ?
+                <div className='flex justify-evenly w-full'>
+                    <Link to ={ `/update/${place._id}`} class="inline-flex gap-2 items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-400 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      <GrUpdate className='text-base'></GrUpdate>  Update 
                     </Link>
                     <button onClick={() => handleDelete(place._id)} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Delete
+                        <MdOutlineDelete className='text-2xl'></MdOutlineDelete> Delete
                     </button>
-                </div>
+                </div>:
                 
+                <Link to = {`/details/${_id}`} class="inline-flex items-center  px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Details
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </Link>
             }
 
         </div>
