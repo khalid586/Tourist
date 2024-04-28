d64cc609b58a620225795c3082c4ff805e004e99
khalid586 <khalidcomilla58@gmail.com>
2024-04-28 17:54:00 +0600

    card design imporved (imaeg size bug fix)

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -20,18 +20,16 @@ function SingleItem({place , handleDelete}) {
         travelTime,
     } = place; 
 
-    description =description.split(' ').slice(0, 20).join(' ');
+    description =description.split(' ').slice(0, 15).join(' ');
   return (
       
 
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src = {photoUrl} alt="" />
-    </a>
+    <img className='w-full rounded-t-lg' style={{ height:'250px'}} src = {photoUrl} alt="" />
     <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
-        </a>
+        
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+        
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
         <div className='flex gap-2'>
             <Link to = {`/details/${_id}`} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
