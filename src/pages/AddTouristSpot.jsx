26a0211d12ae476f53285c6025fa6121813a77b7
khalid586 <khalidcomilla58@gmail.com>
2024-04-28 11:12:10 +0600

    all info updated

--- src/pages/AddTouristSpot.jsx
+++ src/pages/AddTouristSpot.jsx
@@ -25,10 +25,19 @@ function AddTouristSpot() {
         const name = form.name.value;
         const country = form.country.value;
         const photoUrl = form.photoUrl.value;
+        const location = form.location.value;
+        const avgCost = form.avgCost.value;
+        const travelTime = form.travelTime.value;
+        const visitorsPerYear = form.visitorsPerYear.value;
+        const description = form.description.value;
         const email = user.email;
         const userName = user.displayName;
-        const place = {name,country,photoUrl,email,userName};
-
+    
+        // Extract the values of selected checkboxes and store them in an array
+        const seasonality = Array.from(form.querySelectorAll('input[name="seasonality"]:checked')).map(input => input.value);
+    
+        const place = { name, country, photoUrl, location, avgCost, travelTime, visitorsPerYear, description, email, userName, seasonality };
+    
         form.reset();
 
         fetch('https://b9a10-server-side-khalid586-theta.vercel.app/places/',{
@@ -51,7 +60,7 @@ function AddTouristSpot() {
     }
 
     const active = 'bg-green-400 text-white' , nonActive = 'bg-gray-100 text-black';
-
+    const desc = "Carved by the mighty Colorado River over millions of years, its vast expanse of colorful cliffs and deep gorges stretches for over 275 miles, showcasing nature's raw power and geological beauty. Visitors are treated to breathtaking vistas at every turn, with layers of rock revealing a fascinating chronicle of Earth's history."
     return (
      <>
         <Helmet>
@@ -68,19 +77,67 @@ function AddTouristSpot() {
             {
                 tab === 1 ?
                 <form onSubmit={handleSubmit} className="min-w-80 mx-auto">
-                    <div className="mb-5 ">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Place name</label>
-                        <input type="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="kuakata" required />
+                    <div className="mb-5 flex gap-2">
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Place name</label>
+                            <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Inani Beach" required />
+                        </div>
+                        
+                        <div className="">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Photo Url</label>
+                            <input type="text" name = 'photoUrl' id="photoUrl" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="https://picture.png"  required />
+                        </div>
                     </div>
-                    <div className="mb-5">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
-                        <input type="country" id="country" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Bangladesh" required />
+                    <div className='flex gap-2'>
+                        <div className="mb-5">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
+                            <input type="text" id="location" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Cox's Bazar" required />
+                        </div>
+                        <div className="">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
+                            <input type="text" id="country" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Bangladesh" required />
+                        </div>
                     </div>
 
-                    <div className="mb-5 ">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Photo Url</label>
-                        <input type="text" name = 'photoUrl' id="photoUrl" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="https://picture.png"  required />
+
+                    <div className="mb-5 flex gap-2">
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Average Cost</label>
+                            <input type="number" name = 'avgCost' id="avgCost" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="75000"  required />
+                        </div>
+                        <div className="">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Travel Time (Days)</label>
+                            <input type="number" name = 'travelTime' id="travelTime" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="7"  required />
+                        </div>
                     </div>
+                    <div className="mb-5 ">
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Total visitors/year</label>
+                        <input type="number" name = 'visitorsPerYear' id="visitorsPerYear" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="7000"  required />
+                    </div>
+                    <div className="mb-5 ">
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Short Description</label>
+                        <input type="text" defaultValue={desc} name = 'description' id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" required />
+                    </div>
+
+                    <div className="mb-5">
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seasonality</label>
+                        <div className="flex flex-wrap gap-4">
+                            <label className="inline-flex items-center">
+                                <input type="checkbox" name="seasonality" value="winter" className="form-checkbox h-4 w-4 text-green-500" />
+                                <span className="ml-2 text-sm">Winter</span>
+                            </label>
+                            <label className="inline-flex items-center">
+                                <input type="checkbox" name="seasonality" value="summer" className="form-checkbox h-4 w-4 text-green-500" />
+                                <span className="ml-2 text-sm">Summer</span>
+                            </label>
+                            <label className="inline-flex items-center">
+                                <input type="checkbox" name="seasonality" value="fall" className="form-checkbox h-4 w-4 text-green-500" />
+                                <span className="ml-2 text-sm">Fall</span>
+                            </label>
+                            {/* Add more checkboxes for other seasons */}
+                        </div>
+                    </div>
+
 
                     <button type="submit" className="text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Add Place</button>
                 
--- src/pages/UpdatePage.jsx
+++ src/pages/UpdatePage.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Navigate, useLoaderData, useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import { AuthContext } from '../providers/AuthProvider';
 import { Helmet } from 'react-helmet-async';
@@ -7,77 +7,116 @@ import { Helmet } from 'react-helmet-async';
 function UpdatePage() {
     const data = useLoaderData();
     const navigate = useNavigate();
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
-    const {_id,name,country,photoUrl,email,userName} = data;
-    console.log(name);
+    const { _id, name, country, photoUrl,location, email, userName, avgCost, seasonality, description } = data;
 
-
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
-
 
         const form = e.target;
         const name = form.name.value;
         const country = form.country.value;
         const photoUrl = form.photoUrl.value;
+        const avgCost = form.avgCost.value;
+        const seasonality = Array.from(form.querySelectorAll('input[name="seasonality"]:checked')).map(input => input.value);
 
-        const updatedInfo = {name,country,photoUrl,email:user.email,userName:user.displayName};
-        console.log(updatedInfo);
+        const description = form.description.value;
 
-        fetch(`https://b9a10-server-side-khalid586-theta.vercel.app/update/${_id}`,{
-            method:"PUT",
-            headers:{
-                'content-type':'application/json'
+        const updatedInfo = {
+            name,
+            country,
+            photoUrl,
+            avgCost,
+            seasonality,
+            description,
+            email,
+            userName
+        };
+
+        fetch(`https://b9a10-server-side-khalid586-theta.vercel.app/update/${_id}`, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json'
             },
-            body:JSON.stringify(updatedInfo)
+            body: JSON.stringify(updatedInfo)
         })
-        .then(res => res.json())
-        .then(data =>{
-            console.log(data)
-            if(data.acknowledged){
-                if(data.modifiedCount){
-                    toast.success("Details updated Successfully")
-                    
-                    setTimeout(() =>  navigate('/mylist'),2000)
-                   
-                }else{
-                    toast.error("You haven't changed any information")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+                    if (data.modifiedCount) {
+                        toast.success("Details updated Successfully");
+                        setTimeout(() => navigate('/mylist'), 2000);
+                    } else {
+                        toast.error("You haven't changed any information");
+                    }
                 }
-            }
-        })
-        .catch(error => toast(error))
+            })
+            .catch(error => toast(error));
     }
 
     return (
-     <>
-        <Helmet>
-            <title>Tourist | Update</title>
-        </Helmet>
+        <>
+            <Helmet>
+                <title>Tourist | Update</title>
+            </Helmet>
+            <div className='flex justify-center'>
+                <form onSubmit={handleSubmit} className="min-w-80 mx-auto">
+                    <div className="mb-5 flex gap-2">
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Place name</label>
+                            <input defaultValue={name} type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" required />
+                        </div>
+                        <div className="">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Photo Url</label>
+                            <input defaultValue={photoUrl} type="text" name='photoUrl' id="photoUrl" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="https://picture.png" required />
+                        </div>
+                    </div>
+                    <div className='flex gap-2'>
+                        <div className="mb-5">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
+                            <input defaultValue={location} type="text" id="location" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Cox's Bazar" required />
+                        </div>
+                        <div className="">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
+                            <input defaultValue={country} type="text" id="country" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Bangladesh" required />
+                        </div>
+                    </div>
+                    <div className="mb-5 flex gap-2">
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Average Cost</label>
+                            <input defaultValue={avgCost} type="number" name='avgCost' id="avgCost" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="75000" required />
+                        </div>
+                        <div className="">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seasonality</label>
+                            <div className="flex flex-wrap gap-4">
+                                <label className="inline-flex items-center">
+                                    <input type="checkbox" defaultChecked={seasonality.includes('winter')} name="seasonality" value="winter" className="form-checkbox h-4 w-4 text-green-500" />
+                                    <span className="ml-2 text-sm">Winter</span>
+                                </label>
+                                <label className="inline-flex items-center">
+                                    <input type="checkbox" defaultChecked={seasonality.includes('fall')} name="seasonality" value="fall" className="form-checkbox h-4 w-4 text-green-500" />
+                                    <span className="ml-2 text-sm">Fall</span>
+                                </label>
 
-        <div className='flex justify-center'>
-                <form onSubmit={handleSubmit} className="w-full m-2 md:w-1/3">
+                                <label className="inline-flex items-center">
+                                    <input type="checkbox" defaultChecked={seasonality.includes('summer')} name="seasonality" value="summer" className="form-checkbox h-4 w-4 text-green-500" />
+                                    <span className="ml-2 text-sm">Summer</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
                     <div className="mb-5 ">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Place name</label>
-                        <input defaultValue={name} type="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"  required />
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Short Description</label>
+                        <input defaultValue={description} type="text" name='description' id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" required />
                     </div>
-                    <div className="mb-5">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
-                        <input defaultValue={country}  type="country" id="country" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Bangladesh" required />
-                    </div>
-
-                    <div className="mb-5 ">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Photo Url</label>
-                        <input defaultValue={photoUrl} type="text" name = 'photoUrl' id="photoUrl" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="https://picture.png"  required />
-                    </div>
-
                     <button type="submit" className="text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Update</button>
-                
                 </form>
-                <ToastContainer></ToastContainer>
-        </div>
-     </>
-    )
+                <ToastContainer />
+            </div>
+        </>
+    );
 }
 
-export default UpdatePage
+export default UpdatePage;
